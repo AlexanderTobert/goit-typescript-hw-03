09213f4fe66669114decdbb08fd76eeb2347fe2f
AlexanderTobert <tobert.sasha@gmail.com>
2024-02-19 11:22:07 +0200

    complete tascs

--- src/2.ts
+++ src/2.ts
@@ -16,14 +16,14 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+  // name: string;
+  // department: string;
+  // salary: number;
 
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
+  constructor(public name: string, public department: string, public salary: number) {
+    // this.name = name;
+    // this.department = department;
+    // this.salary = salary;
   }
 
   getEmployeeDetails() {
@@ -33,6 +33,9 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(public name: string, public department: string, public salary: number) {
+    super(name, department, salary + 10000);
+  }
 }
 
 
